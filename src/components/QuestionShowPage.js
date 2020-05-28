7331c9308e249aa79e20a00a861f3a6f55f829c9
Hano <hinsul3113@gmail.com>
2020-05-28 15:00:47 -0700

    Add demo for QuestionShowpage goes to class

--- src/components/QuestionShowPage.js
+++ src/components/QuestionShowPage.js
@@ -1,21 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 
 import { QuestionDetails } from "./QuestionDetails";
 import { AnswerList } from "./AnswerList";
 import oneQuestionData from "../oneQuestionData";
 
 // QuestionShowPage component
-export const QuestionShowPage = () => {
-  return (
-    <main className="QuestionShowPage">
-      <QuestionDetails
-        title="What is your favourite color?"
-        body="Red, green, blue, magenta, etc."
-        author={{ full_name: "Michael Owen" }}
-        view_count={100}
-        created_at={new Date()}
-      />
-      <AnswerList answers={oneQuestionData.answers} />
-    </main>
-  );
-};
+export class QuestionShowPage extends Component {
+  render() {
+    return (
+      <main className="QuestionShowPage">
+        <QuestionDetails
+          title="What is your favourite color?"
+          body="Red, green, blue, magenta, etc."
+          author={{ full_name: "Michael Owen" }}
+          view_count={100}
+          created_at={new Date()}
+        />
+        <AnswerList answers={oneQuestionData.answers} />
+      </main>
+    );
+  }
+}
