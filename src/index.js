febfe95e985b4900258948ca23d9031f50b6b97b
Hano <hinsul3113@gmail.com>
2020-05-28 11:58:03 -0700

    Add demo for QuestionDetails

--- src/index.js
+++ src/index.js
@@ -3,9 +3,32 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
+// A react component is a function that returns a react element.
+// PascalCase is the naming convention for react components.
+// Components whose names does not begin with a capital letter,
+// will be interpreted as a plain HTML tag.
+
+const QuestionDetails = () => {
+  return (
+    <div>
+      <h2>What is your favourite color?</h2>
+      <p>
+        Red, green, blue, magenta, etc. <br />
+        By Michael Owen
+      </p>
+      <p>
+        <small>Seen 10 times - Created 10 days ago</small>
+      </p>
+    </div>
+  );
+};
+
+// In JSX, self-closing tags must be closed. <img> doesn't work, you
+// must write <img /> instead.
+
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+    <QuestionDetails />
   </React.StrictMode>,
   document.getElementById("root")
 );
