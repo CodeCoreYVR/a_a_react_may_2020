01568a38f52b804945777ca7e61e13e7f7e1b609
Hano <hinsul3113@gmail.com>
2020-05-29 14:49:38 -0700

    Add demo for Hacky Sign In

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,31 @@ import { QuestionShowPage } from "./QuestionShowPage";
 import { QuestionIndexPage } from "./QuestionIndexPage";
 import { WelcomePage } from "./WelcomePage";
 import { NavBar } from "./NavBar";
+import { Session } from "../api/session";
 
 // In react application, we create a component that acts as the
 // "root" or the entry point to all f our other components.
 // This is the one that should be rendered by `ReactDOM.render()`
 export class App extends React.Component {
   state = {
+    currentUser: null,
     showTime: true,
   };
+
+  componentDidMount() {
+    // This gives us a cookie that represents us being logged in
+    // Now, when we make POST request to the server to create a Question,
+    // we will be authorized/authenticated
+    // This is a HACKY method unril we learn about Authentication in react
+    Session.create({
+      email: "js@winterfell.gov",
+      password: "supersecret",
+    }).then((user) => {
+      this.setState({
+        currentUser: user,
+      });
+    });
+  }
   render() {
     return (
       <BrowserRouter>
